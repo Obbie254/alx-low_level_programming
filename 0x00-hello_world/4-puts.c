43e206258113c200cdc1daae709b9cff1a3d6399
Obbie254 <obbienjehia@gmail.com>
2022-10-18 11:18:37 -0700

    err correction 001

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+/* Fxn that uses puts() to print.
+ * Fxn return zero.
+ */
 int main()
 {
   /* initializing string */
-  char str[] = "script that compiles a C file and creates an executable named cisfun.";
+  char str[] = "\"Programming is like building a multilingual puzzle";
 
   /* writing to stdout */
   puts(str);
