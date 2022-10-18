fea4f3ef1bf6e69a4b3d3a1b070932b8cac2041e
Obbie254 <obbienjehia@gmail.com>
2022-10-18 11:07:27 -0700

    created a puts program.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  /* initializing string */
+  char str[] = "script that compiles a C file and creates an executable named cisfun.";
+
+  /* writing to stdout */
+  puts(str);
+
+  return(0);
+}
