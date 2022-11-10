19b9c6596a79617968a9a2748632797e8db2618a
Obbie254 <obbienjehia@gmail.com>
2022-11-10 00:41:51 -0800

    ammending task 101.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
  * print_number - Prints an integer
- *
  * @n: the integer to print
- *
  * Return: nothing
  */
 
@@ -15,7 +13,7 @@ void print_number(int n)
 		n *= -1;
 	}
 
-	if (n / 10)
+	if (n / 10 != 0)
 		print_number(n / 10);
 
 	_putchar((n % 10) + '0');
