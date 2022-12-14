f4e90c9f7a741bb4f8eb09dcce9520f99dc00006
Obbie254 <obbienjehia@gmail.com>
2022-12-14 06:02:19 -0800

    task 3 done

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * factorial - fxn that returns the factorial of a given number
+ * @n: the variable calculated
+ * Return: 1 for success
+ */
+int factorial(int n)
+{
+	if (n < 0) return (-1);
+	if (n == 0) return (1);
+	return (n * factorial(n - 1));
+}
