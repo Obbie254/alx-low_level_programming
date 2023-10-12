f5bea8154c8f6a5b35c927c11f0a130e86a423fb
obbie254 <obbienjehia@gmail.com>
2023-10-13 02:02:12 +0300

    betty errors correction on file 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,18 @@
 #include "main.h"
 /**
+ * rep - prints xcters
+ * @x: no. of times
+ */
+void rep(int x)
+{
+	_putchar(',');
+	while (x > 0)
+	{
+		_putchar(' ');
+		x--;
+	}
+}
+/**
  * print_times_table - fxn prints n times table
  * @n: number of times
  * Return: 0 if not in range 0<n<15
@@ -8,9 +21,7 @@ void print_times_table(int n)
 {
 	int i, j, d;
 
-	if (n > 15 || n < 0)
-		return;
-	else
+	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i < n + 1; i++)
 		{
@@ -20,34 +31,23 @@ void print_times_table(int n)
 				if (d == 0)
 				{
 					if (j > 0 && j < n + 1)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
+						rep(3);
 					_putchar(d + '0');
 				}
 				else if (d < 10 && j != 0)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
+					rep(3);
 					_putchar(d + '0');
 				}
 				else if (d >= 10 && d < 100)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
+					rep(2);
 					_putchar((d / 10) + '0');
 					_putchar((d % 10) + '0');
 				}
 				else if (d >= 100)
 				{
-					_putchar(',');
-					_putchar(' ');
+					rep(1);
 					_putchar((d / 100) + '0');
 					_putchar(((d % 100) / 10) + '0');
 					_putchar(((d % 100) % 10) + '0');
