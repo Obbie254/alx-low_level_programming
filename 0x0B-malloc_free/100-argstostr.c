10b019452f741c0ad7e61545136891989c706a76
Obbie254 <obbienjehia@gmail.com>
2023-11-01 05:23:47 +0300

    created prog args and its files

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* *argstostr - the fxn concatenates all the arguments
+* @ac : arg of counter
+;* @av : arg of vector
+* Return: pointer
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int x, y;
+	int a = 0;
+	int str = 0;
+	char *d;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (x = 0 ; x < ac ; x++)
+	{
+		for (y = 0 ; av[x][y] != '\0' ; y++)
+		{
+			str++;
+		}
+		str++;
+	}
+
+	d = malloc(sizeof(char) * str);
+
+	if (d == NULL)
+		return (NULL);
+
+	for (x = 0 ; x < ac ; x++)
+	{
+		for (y = 0 ; av[x][y] != '\0' ; y++)
+		{
+			d[a] = av[x][y];
+			a++;
+		}
+		d[a] = '\n';
+		a++;
+	}
+	return (d);
+}
