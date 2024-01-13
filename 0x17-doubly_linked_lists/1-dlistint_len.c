d22182bd96f8d850a1926e9f6bf5f9b86638ef15
Obbie254 <obbienjehia@gmail.com>
2024-01-13 04:38:50 +0300

    debugging done in file 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,14 +9,13 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-size_t i = 0;
-const dlistint_t *tmp = h;
+	size_t i = 0;
+	const dlistint_t *tmp = h;
 
-while (tmp != NULL)
-{
-tmp = tmp->next;
-i++;
-}
-return (i);
-
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
 }
