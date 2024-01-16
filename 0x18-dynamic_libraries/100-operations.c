f1c7f82c054ef28074471db63f54d2c46d48ee40
Obbie254 <obbienjehia@gmail.com>
2024-01-16 22:38:23 +0300

    created a library that can be called by py code

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include "100-operations.h"
+
+/**
+ * add - operation fxn
+ * @a: first number
+ * @b: second number
+ * Return: results
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - operation fxn
+ * @a: first number
+ * @b: second number
+ * Return: results
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - operation fxn
+ * @a: first number
+ * @b: second number
+ * Return: results
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - operation fxn
+ * @a: first number
+ * @b: second number
+ * Return: results
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - operation fxn
+ * @a: first number
+ * @b: second number
+ * Return: results
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
