188ddd6fea898f5de75e3e241ad74ab255178202
Obbie254 <obbienjehia@gmail.com>
2024-01-16 07:30:56 +0300

    debugging done on codefile 103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,160 +1,78 @@
 #include "main.h"
 
 /**
- * infinite_add - Adds two numbers given as a string
- * @n1: 1st given number
- * @n2: 2nd given number
- * @r: Buffer for result storage
- * @size_r: Size of buffer
- *
- * Description: added each number digit in the strings n1 and n2
- * individually from right to left by manipulating the ASCII values to
- * always give the same result as if it was an int addition.
- * ASCII addition = n1[i] + (n2[i] - 48) + carry. if addition > 9,
- * subtract 10 and carry 1 to next digit. if addition <= 9, value is
- * same and carry is zero.
- * Return: Pointer to buffer
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1, len2, maxlen, i = 0, carry = 0, temp;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
 	while (*(n1 + i) != '\0')
-	{
 		i++;
-	}
-	len1 = i;
-	i = 0;
-	while (*(n2 + i) != '\0')
-	{
-		i++;
-	}
-	len2 = i;
-	i = 1;
-
-	/* get length of max string length */
-	if (len1 >= len2)
-		maxlen = len1;
-	else
-		maxlen = len2;
-
-	/*accounts 4 '\0' & 1 xtra digit at zero index 4 last carry if any*/
-	if (maxlen + 1 >= size_r)
-	{
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
 		return (0);
-	}
-
-	/*set d null terminator for r, uses r[0]-r[maxlen] to store ans*/
-	*(r + (maxlen + 1)) = '\0';
-
-	/** start 4rm right of n1[len1 - 1] & n2[len2 - 1] */
-	for (i = 1; i <= maxlen; i++)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		/* get ASCII addition */
-		temp = n1[len1 - i] + (n2[len2 - i] - 48) + carry;
-
-		/* check 4 digit end for any/both n1 & n2 */
-		if (len1 == i || len2 == i)
-		{
-			/*n1, n2 have reached end and len1 == len2 */
-			if (len1 == i && len2 == i)
-			{
-				if (temp > 57) /* sum > 9 */
-				{
-					*(r + (maxlen - i + 1)) = temp - 10;
-					/* carry 1 to r[0] */
-					*(r + (maxlen - i)) = 49;
-				}
-				else
-				{
-					*(r + (maxlen - i + 1)) = temp;
-					*(r + (maxlen - i)) = 48;
-				}
-				break;
-			}
-			if (len1 == i) /* for len2 > len1 */
-			{
-				if (temp > 57)
-				{
-					*(r + (maxlen - i + 1)) = temp - 10;
-					carry = 1;
-				}
-				else
-				{
-					*(r + (maxlen - i + 1)) = temp;
-					carry = 0;
-				}
-				while (len2 != i)
-				{
-					i++;
-					temp = n2[len2 - i] + carry;
-					if (temp > 57)
-					{
-						*(r + (maxlen - i + 1)) =
-							temp - 10;
-						carry = 1;
-					}
-					else
-					{
-						*(r + (maxlen - i + 1)) =
-							temp;
-						carry = 0;
-					}
-				}
-
-				*(r + (maxlen - i)) = carry + 48;
-				break;
-			}
-			if (len2 == i) /* for len1 > len2 */
-			{
-				if (temp > 57)
-				{
-					*(r + (maxlen - i + 1)) = temp - 10;
-					carry = 1;
-				}
-				else
-				{
-					*(r + (maxlen - i + 1)) = temp;
-					carry = 0;
-				}
-				while (len1 != i)
-				{
-					i++;
-					temp = n1[len1 - i] + carry;
-					if (temp > 57)
-					{
-						*(r + (maxlen - i + 1)) =
-							temp - 10;
-						carry = 1;
-					}
-					else
-					{
-						*(r + (maxlen - i + 1)) =
-							temp;
-						carry = 0;
-					}
-				}
-				*(r + (maxlen - i)) = carry + 48;
-				break;
-			}
-		}
+		if (i < 0)
+			val1 = 0;
 		else
-		{
-			if (temp > 57)
-			{
-				*(r + (maxlen - i + 1)) = temp - 10;
-				carry = 1;
-			}
-			else
-			{
-				*(r + (maxlen - i + 1)) = temp;
-				carry = 0;
-			}
-		}
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-	if (*r == '0')
-		return (r + 1);
-	else
-		return (r);
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
