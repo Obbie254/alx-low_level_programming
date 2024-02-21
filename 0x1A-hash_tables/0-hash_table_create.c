9244ae33492014c0c61ec511c6fecf54464e1801
Obbie254 <obbienjehia@gmail.com>
2024-02-21 18:07:25 +0300

    modifications done on file 0 and readme file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,13 +17,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = malloc(size * sizeof(hash_table_t *));
+	table->array = malloc(sizeof(hash_table_t *) * size);
 	if (table->array == NULL)
 	{
 		free(table);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++);
+	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 	return (table);
 }
