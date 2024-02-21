23014688e9259cdfe47844e23706a770d4a1fff4
Obbie254 <obbienjehia@gmail.com>
2024-02-21 17:34:13 +0300

    debug done on file o

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,11 +12,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-	table = calloc(1, sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = calloc(size, sizeof(hash_table_t *));
+	table->array = malloc(size * sizeof(hash_table_t *));
 	if (table->array == NULL)
 	{
 		free(table);
