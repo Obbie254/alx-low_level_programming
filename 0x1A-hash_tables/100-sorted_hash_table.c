58342e5c2d5f355fd280ada5f170893fb5223242
Obbie254 <obbienjehia@gamil.com>
2024-08-28 13:18:21 +0300

    debug4 done on file 100

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -186,7 +186,7 @@ void shash_table_print_rev(const shash_table_t *ht)
 	{
 		printf("'%s': '%s'", node->key, node->value);
 		node = node->sprev;
-		if (node != NUL)
+		if (node != NULL)
 			printf(", ");
 	}
 	printf("}\n");
@@ -216,4 +216,4 @@ void shash_table_delete(shash_table_t *ht)
 
 	free(head->array);
 	free(head);
-}L
+}
